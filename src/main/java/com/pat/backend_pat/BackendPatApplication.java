45d7751f1df52184953f6a99dc0f13fdf1731cf9
B S Aishwarya <yadavaishwarya114@gmail.com>
2026-03-10 17:53:11 -0500

    Add DTO classes and JWT utility

---
+++ src/main/java/com/pat/backend_pat/BackendPatApplication.java
@@ -0,0 +1,13 @@
+package com.pat.backend_pat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPatApplication.class, args);
+	}
+
+}
