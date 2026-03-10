b62cfa73a4eaa2ef0ffbc68d36bb510075266708
Mohammed Fahim R <mohammedfahim2204@gmail.com>
2026-03-10 11:11:36 +0530

    Initial Spring Boot backend skeleton

---
+++ backend_pat/src/test/java/com/pat/backend_pat/BackendPatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pat.backend_pat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
