df0a8180150c4275742db6691613d1c98da29790
B S Aishwarya <yadavaishwarya114@gmail.com>
2026-03-17 15:50:20 -0500

    Added EmployerRepository

---
+++ backend_pat/src/main/java/com/pat/backend_pat/repository/EmployerRepository.java
@@ -0,0 +1,9 @@
+package com.pat.backend_pat.repository;
+
+
+
+import com.pat.backend_pat.entity.Employer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployerRepository extends JpaRepository<Employer, Long> {
+}
