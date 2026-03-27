d3ff95157fe8363df631926cfd3348d985ee8332
aishwarya2aish <ai.16shwarya@gmail.com>
2026-03-27 15:27:26 +0530

    fix: add findByStudentStudentId method to ResumeRepository

--- backend_pat/src/main/java/com/pat/backend_pat/repository/ResumeRepository.java
+++ backend_pat/src/main/java/com/pat/backend_pat/repository/ResumeRepository.java
@@ -1,4 +1,5 @@
 package com.pat.backend_pat.repository;
+import java.util.List;
 
 import com.pat.backend_pat.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResumeRepository extends JpaRepository<Resume, Integer> {
+	List<Resume> findByStudentStudentId(Integer studentId);
 }
