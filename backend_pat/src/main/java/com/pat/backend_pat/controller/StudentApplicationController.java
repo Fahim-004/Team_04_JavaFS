5e149f99224c8ab43008fad207c6200dc9194c77
Manoj K <manojk203017@gmail.com>
2026-04-07 13:19:22 +0530

    added conroller file

--- backend_pat/src/main/java/com/pat/backend_pat/controller/StudentApplicationController.java
+++ backend_pat/src/main/java/com/pat/backend_pat/controller/StudentApplicationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
 public class StudentApplicationController {
+	
 
     @Autowired
     private ApplicationService applicationService;
