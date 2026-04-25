f59b890ff7f424fc28eb7157d7c53a8d53dcae59
Narasimha-M-K <narasimhamk2004@gmail.com>
2026-04-25 13:05:08 +0530

    chore(backend): update local db config and encoder helper

---
+++ backend_pat/src/main/java/com/util/test.java
@@ -0,0 +1,10 @@
+package com.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class test {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin123"));
+    }
+}
